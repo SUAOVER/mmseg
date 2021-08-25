2871bb38f6f59404ef0d94f46336352ae1e87082
Your Name <you@example.com>
2021-08-25 15:16:52 +0000

    fuck  sbc 0825

--- mmseg/datasets/voc.py
+++ mmseg/datasets/voc.py
@@ -3,7 +3,7 @@ import os.path as osp
 from .builder import DATASETS
 from .custom import CustomDataset
 
-
+'''
 @DATASETS.register_module()
 class PascalVOCDataset(CustomDataset):
     """Pascal VOC dataset.
@@ -22,6 +22,26 @@ class PascalVOCDataset(CustomDataset):
                [192, 0, 0], [64, 128, 0], [192, 128, 0], [64, 0, 128],
                [192, 0, 128], [64, 128, 128], [192, 128, 128], [0, 64, 0],
                [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128]]
+
+    def __init__(self, split, **kwargs):
+        super(PascalVOCDataset, self).__init__(
+            img_suffix='.jpg', seg_map_suffix='.png', split=split, **kwargs)
+        assert osp.exists(self.img_dir) and self.split is not None
+'''
+
+
+
+@DATASETS.register_module()
+class PascalVOCDataset(CustomDataset):
+    """Pascal VOC dataset.
+
+    Args:
+        split (str): Split txt file for Pascal VOC.
+    """
+
+    CLASSES = ('background', 'sbc')
+
+    PALETTE = [[0, 0, 0], [128, 0, 0]]
 
     def __init__(self, split, **kwargs):
         super(PascalVOCDataset, self).__init__(
